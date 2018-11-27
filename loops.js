f2c2057067964c92eb36f99adf08718b3efdc0b4
Nazrul Babu <n12babu@siena.edu>
2018-11-27 21:33:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 
 const forLoop= (array)=>{
   for (let i=0; i<25; i++){
-    if (i===1){ 
+    if (i===1){
     return array.push(`"I am 1 strange loop."`)
 
     } else{ return array.push(`"I am ${i} strange loops."`)
